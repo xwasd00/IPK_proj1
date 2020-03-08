366ecc378d550f73073ca7d07c03cbcb8b646cd5
xwasd00 <xsovam00@stud.fit.vutbr.cz>
2020-03-08 19:31:26 +0100

    dodelana dokumentace

--- readme.md
+++ readme.md
@@ -6,4 +6,8 @@ autor: Michal Sova (xsovam00)
 - Příklad užití pomocí příkazu curl
 > $ curl localhost:5353/resolve?name=www.seznam.cz\&type=A
 ## Popis  řešení
-
+Cílem projektu je implementace severu, který komunikuje protokolem HTTP a zajišťuje překlad doménových jmen. Pro překlad jmen server používá resolver metody socket - socket.gethostbyname() a socket.gethostbyaddr() ve funkci resolve(n, t, to_send).
+### GET
+Operace GET se zpracovává ve funkci get, která vrací zprávu k odeslání klientovi. Ve funkci se kontroluje validnost hlavičky a rozparsovává se URL požadavek. Typ a jméno se posílá do funkce resolve, která (pokud je to možné) provede překlad.
+### POST
+Operace POST se zpracovává ve funkci post, která vrací příslušnou zprávu k odeslání klientovi. Ve funkci se kontroluje validnost hlavičky a zpracovávají se URL požadavky v těle zprávy. Každý řádek se rozparsovává a zjišťuje se, jestli je validní. Typ a jméno se poté posílá do funkce resolve, která (pokud je to možné) provede překlad. Je-li řádek chybný (špatná doména, chybný formát) řádek se ignoruje.
--- src/resolver.py
+++ src/resolver.py
@@ -3,30 +3,48 @@ import re
 import threading
 import sys
 
+# hlavickove http odpovedi
 bad_request = b'HTTP/1.1 400 Bad Request\r\n\r\n'
 not_found = b'HTTP/1.1 404 Not Found\r\n\r\n'
 method_not_allowed = b'HTTP/1.1 405 Method Not Allowed\r\n\r\n'
 ok = b'HTTP/1.1 200 OK\r\n\r\n'
 
+## funkce resolve preklada domenova jmena na adresu  naopak, 
+#  pripojuje odpoved v zadanem tvaru ke zbytku zpravy k odeslani
+#  parametry: n = jmeno(domenove) nebo adresa, t = typ (A nebo PTR), to_send = zprava k odeslani
 def resolve(n, t, to_send):
     if t == "A" and not re.findall(r"(\d+.\d+.\d+.\d+)", n):
         try:
             ret = socket.gethostbyname(n)
         except socket.error:
-            if to_send == ok or to_send == not_found:
+            if to_send == ok or to_send == not_found: 
+                # kvuli funkci post -> pokud ve funkci post zatim neni ani jeden radek spravny,
+                # vrati not found, jinak pripoji ke zprave (to_send) prelozene jmeno
                 return not_found
             return to_send
+        ## try except 
+        if to_send == not_found:
+            to_send = ok # post -> adresa/jmeno nalezeno
         to_send += bytes(n, "UTF-8") + b':A=' + bytes(ret, "UTF-8") + b'\r\n'
     elif t == "PTR" and re.findall(r"(\d+.\d+.\d+.\d+)", n):
         try:
             a,b,ret = socket.gethostbyaddr(n)
         except socket.error:
-            if to_send == ok or to_send == not_found:
+            if to_send == ok or to_send == not_found: 
+                # kvuli funkci post -> pokud ve funkci post zatim neni ani jeden radek spravny,
+                # vrati not found, jinak pripoji ke zprave (to_send) prelozene jmeno
                 return not_found
             return to_send
+        ## try except
+        if to_send == not_found:
+            to_send = ok #post ->adresa/jmeno nalezeno
         to_send += bytes(n, "UTF-8") + b':PTR=' + bytes(a, "UTF-8") + b'\r\n'
     return to_send
 
+
+## funkce pro operaci GET
+#  zjisti, jestli je dotaz ve spravnem formatu a pomoci 
+#  funkce resolve vrati prislusnou odpoved serveru
 def get(header, to_send):
     arr = header.split("\r\n")
     line = re.findall(r"GET (\S+) HTTP/", arr[0])
@@ -35,22 +53,15 @@ def get(header, to_send):
         my_type = re.findall(r"/resolve\?name=(?:\S+)&type=(A|PTR)", line[0])
         if my_name and my_type:
             to_send = ok
-            if my_type[0] == "A" and not re.findall(r"(\d+.\d+.\d+.\d+)", my_name[0]):
-                try:
-                    ret = socket.gethostbyname(my_name[0])
-                except socket.error:
-                    return not_found
-                to_send += bytes(my_name[0], "UTF-8") + b':A=' + bytes(ret, "UTF-8") + b'\r\n'
-            elif my_type[0] == "PTR" and re.findall(r"(\d+.\d+.\d+.\d+)", my_name[0]):
-                try:
-                    a,b,ret = socket.gethostbyaddr(n)
-                except socket.error:
-                    return not_found
-                to_send += bytes(my_name[0], "UTF-8") + b':PTR=' + bytes(a, "UTF-8") + b'\r\n'
-            else:
+            to_send = resolve(my_name[0], my_type[0], to_send)
+            if to_send == ok:
                 return bad_request
     return to_send
 
+
+## funkce pro operaci POST
+#  zjisti, jestli je dotaz ve spravnem formatu a pomoci funkce resolve 
+## zpracovava zpravu radek po radku
 def post(header, body, to_send):
     arr = header.split("\r\n")
     if re.findall(r"(POST /dns-query HTTP/)", arr[0]):
@@ -61,19 +72,18 @@ def post(header, body, to_send):
             my_type = re.findall(r"\s*(?:[^:\s]+)\s*:\s*(A|PTR)\s*", line)
             if my_name and my_type:
                 to_send = resolve(my_name[0], my_type[0], to_send)
-        if (to_send == ok):
+        if to_send == ok:
             return bad_request
     return to_send
-
-
+  
 def length(header):
     match = re.findall(r"Content-Length: (\d+)",header)
     if match:
         return int(match[0])
     return 0
 
-
 if __name__ == "__main__":
+    # kontrola argumentu
     if len(sys.argv) < 2:
         sys.stderr.write("Málo argumentů. Použití: $python3 resolver.py PORT\n")
         sys.exit(1)
@@ -86,8 +96,9 @@ if __name__ == "__main__":
 
     HOST = ''
     PORT = int(sys.argv[1])
-    BUFF_SIZE = 4096
+    BUFF_SIZE = 8192
     to_send = bad_request
+    # v pripade, ze se nepodari nabindovat HOST s PORTem, ukonci se program
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)    
@@ -99,22 +110,24 @@ if __name__ == "__main__":
 
     
     while 1:
+        # prijem a obsluha klienta
         conn, addr = s.accept()
         data = conn.recv(BUFF_SIZE)
-        part = data.split(b'\r\n\r\n')
+        part = data.split(b'\r\n\r\n') # oddeleni hlavicky a zbytku zpravy (operace POST)
 
-        if part[0][0:3] == b'GET':
+        
+        if part[0][0:3] == b'GET': # operace GET
             to_send = get( part[0].decode("UTF-8"), to_send )
-        elif part[0][0:4] == b'POST':
+        elif part[0][0:4] == b'POST': #operace POST (+kontrola delky zpravy, pokud je v hlavicce)
             l = length(part[0].decode("UTF-8"))
             if (l - BUFF_SIZE > 512):
                 data = conn.recv(l)
                 part[1] += data
             to_send = post(part[0].decode("UTF-8"), part[1].decode("UTF-8"), to_send)
         else:
-            #bad request
+            #spatna operace (ani GET ani POST)
             to_send = method_not_allowed
-   
+        # poslani zpravy a ukonceni spojeni
         conn.sendall(to_send)
         conn.close()
 
